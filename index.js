1e317c4da6c2fc1b8807bae177b20871a40df311
amanharness <122996901+amanharness@users.noreply.github.com>
2023-11-21 08:27:46 +0530

    Improved Readability of the code

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+
 const Note = require("./models/Note");
 const User = require("./models/User");
-const path = require("path");
+
 const app = express();
 const port = 3000;
-app.use(express.json({ extended: true }));
-app.use(express.urlencoded());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose
   .connect("mongodb://localhost:27017")
@@ -17,6 +20,7 @@ mongoose
     console.error("Error connecting to MongoDB:", error);
   });
 
+// Serve static HTML files
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "pages", "index.html"));
 });
@@ -29,27 +33,29 @@ app.get("/signup", (req, res) => {
   res.sendFile(path.join(__dirname, "pages", "signup.html"));
 });
 
+// User authentication routes
 app.post("/login", async (req, res) => {
   const { userToken } = req.body;
-  let user = await User.findOne(req.body);
-  console.log(user);
-  if (!user) {
-    res.status(200).json({ success: false, message: "No user found" });
-  } else {
-    res
-      .status(200)
-      .json({
+  try {
+    let user = await User.findOne(req.body);
+    if (!user) {
+      res.status(200).json({ success: false, message: "No user found" });
+    } else {
+      res.status(200).json({
         success: true,
         user: { email: user.email },
-        message: "No user found",
+        message: "Login successful",
       });
+    }
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
   }
 });
+
 app.post("/signup", async (req, res) => {
   const { userToken } = req.body;
-  console.log(req.body);
   try {
-    // Create a new user in the database
     let user = await User.create(req.body);
     res.status(200).json({ success: true, user: user });
   } catch (error) {
@@ -57,49 +63,56 @@ app.post("/signup", async (req, res) => {
     res.status(500).json({ success: false, error: "Internal Server Error" });
   }
 });
+
+// Note-related routes
 app.post("/addnote", async (req, res) => {
   const { userToken } = req.body;
-  // Create a new note in the database
-  let note = await Note.create(req.body);
-  res.status(200).json({ success: true, note });
+  try {
+    let note = await Note.create(req.body);
+    res.status(200).json({ success: true, note });
+  } catch (error) {
+    console.error("Error adding note:", error);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
 });
+
 app.post("/getnotes", async (req, res) => {
   const { userToken } = req.body;
-  let notes = await Note.find({ email: req.body.email });
-  res.status(200).json({ success: true, notes });
+  try {
+    let notes = await Note.find({ email: req.body.email });
+    res.status(200).json({ success: true, notes });
+  } catch (error) {
+    console.error("Error retrieving notes:", error);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
 });
-// Add a new route for deleting a note
+
 app.delete("/deletenote/:id", async (req, res) => {
   const { userToken } = req.body;
   const noteId = req.params.id;
 
   try {
-    // Find the note by ID and delete it
-    const deletedNote = await Note.findByIdAndDelete(noteId);
-
+    let deletedNote = await Note.findByIdAndDelete(noteId);
     if (!deletedNote) {
-      return res
-        .status(404)
-        .json({ success: false, message: "Note not found" });
+      return res.status(404).json({ success: false, message: "Note not found" });
     }
 
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "Note deleted successfully",
-        deletedNote,
-      });
+    res.status(200).json({
+      success: true,
+      message: "Note deleted successfully",
+      deletedNote,
+    });
   } catch (error) {
     console.error("Error deleting note:", error);
     res.status(500).json({ success: false, error: "Internal Server Error" });
   }
 });
 
+// Serve about.html
 app.get('/about', (req, res) => {
   const filePath = path.join(__dirname, 'about.html');
   console.log('Attempting to send file from path:', filePath);
-  
+
   res.sendFile(filePath, (err) => {
     if (err) {
       console.error('Error sending file:', err);
